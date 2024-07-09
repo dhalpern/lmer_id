e94ebb7fed0b50c26fb9b9051a4b1888be7175ea
dhalpern <djh389@nyu.edu>
2024-07-09 12:08:27 -0400

    edits for clarity

--- lmer_rid_experiments.R
+++ lmer_rid_experiments.R
@@ -5,19 +5,19 @@ library(rsvd)
 m0 <- lmer(sottrunc2 ~ 1 + T + P + F + TP + TF + PF + TPF + 
              (1 + T + F + TF | session) + (1 + T + P + TP | item), 
            gb12, REML = FALSE)
-summary(rePCA(m0))
+summary(rePCA(m0))  # both item and session have rank 2
 
 # Do RID on both ----------------------------------------------------------
 
 chf0_session <- getME(m0, "Tlist")[[1]]
 (xx_session <- tcrossprod(chf0_session))
-id_out_session <- rid(xx_session, k = dim(xx_session)[1] - 2, mode='column')
-id_out_session$idx
+id_out_session <- rid(xx_session, k = 2)
+id_out_session$idx # 1 and 3
 
 chf0_item <- getME(m0, "Tlist")[[1]]
 (xx_item <- tcrossprod(chf0_item))
-id_out_item <- rid(xx_item, k = dim(xx_item)[1] - 2, mode='column')
-id_out_item$idx
+id_out_item <- rid(xx_item, k = 2)
+id_out_item$idx # 1 and 3
 
 m_rid_both <- lmer(
   sottrunc2 ~ 1 + T + P + F + TP + TF + PF + TPF + (1 + F | session) + (1 + P | item), 
@@ -26,35 +26,21 @@ summary(rePCA(m_rid_both))
 
 # Do RID on session first ----------------------------------------------------
 
-chf0_session <- getME(m0, "Tlist")[[1]]
-(xx_session <- tcrossprod(chf0_session))
-id_out_session <- rid(xx_session, k = dim(xx_session)[1] - 2, mode='column')
-id_out_session$idx
-
 m_rid_session <- lmer(
   sottrunc2 ~ 1 + T + P + F + TP + TF + PF + TPF + (1 + F | session) + (1 + T + P + TP | item), 
   gb12, REML = FALSE)
-summary(rePCA(m_rid_session))
+summary(rePCA(m_rid_session)) # item has rank 2
 
-chf0_item <- getME(m_rid_session, "Tlist")[[2]]
-(xx_item <- tcrossprod(chf0_item))
-id_out_item <- rid(xx_item, k = dim(xx_item)[1] - 2, mode='column')
-id_out_item$idx
+chf_rid_item <- getME(m_rid_session, "Tlist")[[2]]
+(xx_rid_item <- tcrossprod(chf_rid_item))
+id_out_rid_item <- rid(xx_rid_item, k = 2)
+id_out_rid_item$idx # This results in the same as before
 
 # Do RID on item first ----------------------------------------------------
-
-chf0_item <- getME(m0, "Tlist")[[1]]
-(xx_item <- tcrossprod(chf0_item))
-id_out_item <- rid(xx_item, k = dim(xx_item)[1] - 2, mode='column')
-id_out_item$idx
 
 m_rid_item <- lmer(
   sottrunc2 ~ 1 + T + P + F + TP + TF + PF + TPF + (1 + T + F + TF | session) + (1 + P | item), 
   gb12, REML = FALSE)
-summary(rePCA(m_rid_item))
+summary(rePCA(m_rid_item)) # item is still not full rank!
 
-chf0_session <- getME(m_rid_item, "Tlist")[[1]]
-(xx_session <- tcrossprod(chf0_session))
-id_out_session <- rid(xx_session, k = dim(xx_session)[1] - 2, mode='column')
-id_out_session$idx
 
