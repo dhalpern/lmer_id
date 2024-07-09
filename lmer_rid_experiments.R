178fe7c475d8cf642c6a962f3604a4e341b888c2
dhalpern <djh389@nyu.edu>
2024-07-09 14:05:51 -0400

    use rank computed by R

--- lmer_rid_experiments.R
+++ lmer_rid_experiments.R
@@ -11,22 +11,22 @@ summary(rePCA(m0))  # both item and session have rank 2
 
 chf0_session <- getME(m0, "Tlist")[[1]]
 (xx_session <- tcrossprod(chf0_session))
-id_out_session <- rid(xx_session, k = 2)
+id_out_session <- rid(xx_session, k = qr(xx_session)$rank)
 id_out_session$idx # 1 and 3
 
 chf0_item <- getME(m0, "Tlist")[[2]]
 (xx_item <- tcrossprod(chf0_item))
-id_out_item <- rid(xx_item, k = 2)
-id_out_item$idx # 1 and 3
+id_out_item <- rid(xx_item, k = qr(xx_item)$rank)
+id_out_item$idx # 1, 2, 3
 
 m_rid_both <- lmer(
-  sottrunc2 ~ 1 + T + P + F + TP + TF + PF + TPF + (1 + F | session) + (1 + P | item), 
+  sottrunc2 ~ 1 + T + P + F + TP + TF + PF + TPF + (1 + F | session) + (1 + T + P | item), 
   gb12, REML = FALSE)
 summary(rePCA(m_rid_both))
 
 chf_rid_both_item <- getME(m_rid_both, "Tlist")[[2]]
 (xx_rid_both_item <- tcrossprod(chf_rid_both_item))
-id_out_rid_both_item <- rid(xx_rid_both_item, k = 1)
+id_out_rid_both_item <- rid(xx_rid_both_item, k = qr(xx_rid_both_item)$rank)
 id_out_rid_both_item$idx
 
 #Final model
@@ -43,8 +43,13 @@ summary(rePCA(m_rid_session)) # item has rank 2
 
 chf_rid_item <- getME(m_rid_session, "Tlist")[[2]]
 (xx_rid_item <- tcrossprod(chf_rid_item))
-id_out_rid_item <- rid(xx_rid_item, k = 2)
-id_out_rid_item$idx # This results in the same as before
+id_out_rid_item <- rid(xx_rid_item, k = qr(xx_rid_item)$rank)
+id_out_rid_item$idx # 1, 3
+
+m_rid_session_item <- lmer(
+  sottrunc2 ~ 1 + T + P + F + TP + TF + PF + TPF + (1 + F | session) + (1 + P | item), 
+  gb12, REML = FALSE)
+summary(rePCA(m_rid_session_item)) # item still not full rank
 
 # Do RID on item first ----------------------------------------------------
 
@@ -73,11 +78,11 @@ summary(rePCA(m_cp))
 
 chf_cp_session <- getME(m_cp, "Tlist")[[1]]
 (xx_cp_session <- tcrossprod(chf_cp_session))
-id_out_cp_session <- rid(xx_cp_session, k = 2)
+id_out_cp_session <- rid(xx_cp_session, k = qr(xx_cp_session)$rank)
 id_out_cp_session$idx # 1 and 3
 
 chf_cp_item <- getME(m_cp, "Tlist")[[2]]
 (xx_cp_item <- tcrossprod(chf_cp_item))
-id_out_cp_item <- rid(xx_cp_item, k = 1)
+id_out_cp_item <- rid(xx_cp_item, k = qr(xx_cp_item)$rank)
 id_out_cp_item$idx # 1
 
